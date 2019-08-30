4b1d2d1bfe38146c4391814cd979da9b58b6732b
Miguel Erja <gomi@xxxlgroup.com>
2019-08-30 12:58:56 +0200

    add custom hooks into tetris component logic

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { createStage } from '../utils/gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+
+import { useStage, useInterval, usePlayer } from '../hooks/index';
 
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
+  const [dropTime, setdropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log('re-renderer');
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
-          <div>
-            <Display text="Score" />
-            <Display text="score" />
-            <Display text="score" />
-          </div>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
           <StartButton />
         </aside>
       </StyledTetris>
---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+import { usePlayer } from './usePlayer';
+import { useStage } from './useStage';
+import { useInterval } from './useInterval';
+
+export { usePlayer, useStage, useInterval };
