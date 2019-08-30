8e409c2d221af5a127fd5d5c4a5327efc4bf0b79
Miguel Erja <gomi@xxxlgroup.com>
2019-08-30 10:05:49 +0200

    set style for tetris component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { createStage } from '../utils/gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 import Stage from './Stage';
 import Display from './Display';
@@ -8,17 +9,19 @@ import StartButton from './StartButton';
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="score" />
-          <Display text="score" />
-          <Display text="score" />
-        </div>
-      </aside>
-      <StartButton />
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="score" />
+            <Display text="score" />
+          </div>
+        </aside>
+        <StartButton />
+      </StyledTetris>
+    </StyledTetrisWrapper>
   )
 };
 
